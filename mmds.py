d26534e46affe0b3cd6453de2759e4ec76df8995
prakash <pkhsekar@gmail.com>
2022-09-26 17:52:56 -0400

    showing partitions with district graph overlay

--- mmds.py
+++ mmds.py
@@ -1,3 +1,4 @@
+import gerrychain
 from gerrychain import Graph, Partition, MarkovChain
 import geopandas as gp
 import matplotlib.pyplot as plt
@@ -9,6 +10,7 @@ import networkx as nx
 
 SHAPE_FILE = "./state_data/PA/PA.shp"
 GRAPH_FILE = "./PAjson_wo_geometry"
+ASSIGNMENT_COL = "CD_2011"
 COLORS = ListedColormap(['#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#46f0f0', '#f032e6', '#bcf60c', '#fabebe', '#008080',
                          '#e6beff', '#9a6324', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000075', '#808080', '#000000'])
 
@@ -28,11 +30,14 @@ def gen_mmd_configs(smd_partition: Partition) -> list[tuple]:
     return configs
 
 
-def merge_smds_to_mmds(smd_partition: Partition, config: tuple) -> Partition:
-    return
+def get_district_centroids(district_geo_df: GeoDataFrame) -> dict[int, tuple]:
+    district_centroids = {}
+    for idx, row in district_geo_df.iterrows():
+        district_centroids[idx] = tuple(row["geometry"].centroid.coords)[0]
+    return district_centroids
 
 
-def reduce_partition_to_district_graph(partition: Partition) -> nx.Graph:
+def gen_district_graph(partition: Partition) -> nx.Graph:
     edgelist = []
     for edge in partition["cut_edges"]:
         edgelist.append(
@@ -40,15 +45,22 @@ def reduce_partition_to_district_graph(partition: Partition) -> nx.Graph:
     return nx.Graph(edgelist)
 
 
+def merge_smds_to_mmds(smd_partition: Partition, config: tuple) -> Partition:
+    return
+
+
 def main() -> None:
-    geo_df: GeoDataFrame = gp.read_file(SHAPE_FILE)
-    print(geo_df.columns)
-    graph: Graph = Graph.from_json(GRAPH_FILE)
+    precinct_geo_df: GeoDataFrame = gp.read_file(SHAPE_FILE)
+    district_geo_df: GeoDataFrame = precinct_geo_df.dissolve(by=ASSIGNMENT_COL)
+    precinct_graph: gerrychain.Graph = Graph.from_json(GRAPH_FILE)
     smd_partition: Partition = Partition(
-        graph,
-        assignment="CD_2011"
-    )
-    print(reduce_partition_to_district_graph(smd_partition))
+        precinct_graph, assignment=ASSIGNMENT_COL)
+    district_graph: nx.Graph = gen_district_graph(smd_partition)
+    district_centroids: dict[int, tuple] = get_district_centroids(
+        district_geo_df)
+    plot_partition(smd_partition, precinct_geo_df, COLORS)
+    nx.draw(district_graph, pos=district_centroids)
+    plt.show()
 
 
 if __name__ == "__main__":
