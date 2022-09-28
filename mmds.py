8d45745dad68036b9fedc4f6a6ed8ee1cadf47ec
prakash <pkhsekar@gmail.com>
2022-09-28 00:30:59 -0400

    renamed vars, removed plot_partition function, using GeoSeries for geometries

--- mmds.py
+++ mmds.py
@@ -3,10 +3,8 @@ import random
 from gerrychain import Graph, Partition, MarkovChain
 import geopandas as gp
 import matplotlib.pyplot as plt
-import numpy as np
 from matplotlib.colors import ListedColormap
-from gerrychain.tree import random_spanning_tree
-from geopandas import GeoDataFrame
+from geopandas import GeoSeries
 import networkx as nx
 
 SHAPE_FILE = "./state_data/PA/PA.shp"
@@ -16,10 +14,6 @@ COLORS = ListedColormap(['#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231',
 '#911eb4', '#46f0f0', '#f032e6', '#bcf60c', '#fabebe', '#008080', '#e6beff',
 '#9a6324', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000075',
 '#808080', '#000000'])
-
-
-def plot_partition(partition: Partition, geometries: GeoDataFrame, colors: ListedColormap) -> None:
-    partition.plot(geometries, cmap=colors)
 
 
 def gen_mmd_configs(smd_partition: Partition) -> list[tuple]:
@@ -33,10 +27,11 @@ def gen_mmd_configs(smd_partition: Partition) -> list[tuple]:
     return configs
 
 
-def get_district_centroids(district_geo_df: GeoDataFrame) -> dict[int, tuple]:
+def get_district_centroids(partition: Partition, precinct_geometries: GeoSeries) -> dict[int, tuple]:
     district_centroids = {}
-    for idx, row in district_geo_df.iterrows():
-        district_centroids[idx] = tuple(row["geometry"].centroid.coords)[0]
+    for part, nodes in partition.parts.items():
+        part_geometries = precinct_geometries.loc[precinct_geometries.index[list(nodes)]]
+        district_centroids[part] = tuple(part_geometries.unary_union.centroid.coords)[0]
     return district_centroids
 
 
@@ -47,11 +42,11 @@ def gen_district_graph(partition: Partition) -> nx.Graph:
     return nx.Graph(edgelist)
 
 
-def district_graph_to_mmds(G: nx.Graph, component_sizes: tuple, rand_cut_iterations: int = 100000, node_repeats: int = 5) -> nx.Graph:
+def partition_district_graph(G: nx.Graph, component_sizes: tuple, cut_iterations: int = 100000, node_repeats: int = 5) -> nx.Graph:
     nparts: int = sum(component_sizes)
     for _ in range(node_repeats):
         spanning_tree: nx.Graph = nx.random_spanning_tree(G)
-        for _ in range(rand_cut_iterations):
+        for _ in range(cut_iterations):
             random_edges: list[tuple] = random.sample(
                 spanning_tree.edges, nparts-1)
             cut_spanning_tree: nx.Graph = spanning_tree.copy()
@@ -60,7 +55,7 @@ def district_graph_to_mmds(G: nx.Graph, component_sizes: tuple, rand_cut_iterati
                      for component in nx.connected_components(cut_spanning_tree)]
             if sizes.count(3) == component_sizes[0] and sizes.count(4) == component_sizes[1] and sizes.count(5) == component_sizes[2]:
                 return cut_spanning_tree
-    raise Exception("partitioning failed after %d random cut iterations after %d node repeats" % (rand_cut_iterations, node_repeats))
+    raise Exception("partitioning failed after %d random cut iterations after %d node repeats" % (cut_iterations, node_repeats))
 
 
 def merge_smds_to_mmds(smd_partition: Partition, config: tuple) -> Partition:
@@ -68,20 +63,17 @@ def merge_smds_to_mmds(smd_partition: Partition, config: tuple) -> Partition:
 
 
 def main() -> None:
-    precinct_geo_df: GeoDataFrame = gp.read_file(SHAPE_FILE)
-    district_geo_df: GeoDataFrame = precinct_geo_df.dissolve(by=ASSIGNMENT_COL)
     precinct_graph: gerrychain.Graph = Graph.from_json(GRAPH_FILE)
+    precinct_geometries: GeoSeries = gp.GeoSeries.from_file(SHAPE_FILE)
     smd_partition: Partition = Partition(precinct_graph, assignment=ASSIGNMENT_COL)
-    district_graph: nx.Graph = gen_district_graph(smd_partition)
-    district_centroids: dict[int, tuple] = get_district_centroids(district_geo_df)
-    print(district_centroids)
-    district_configs: list[tuple] = gen_mmd_configs(smd_partition)
-    print(district_configs)
-    for district_config in district_configs:
+    smd_partition.plot(precinct_geometries, cmap=COLORS)
+    smd_graph: nx.Graph = gen_district_graph(smd_partition)
+    smd_centroids: dict[int, tuple] = get_district_centroids(smd_partition, precinct_geometries)
+    mmd_configs: list[tuple] = gen_mmd_configs(smd_partition)
+    for district_config in mmd_configs:
         print("processing config: %s" % str(district_config))
-        district_partition = district_graph_to_mmds(district_graph, district_config)
-        plot_partition(smd_partition, precinct_geo_df, COLORS)
-        nx.draw(district_partition, pos=district_centroids, node_size=[10]*len(district_graph.nodes))
+        district_partition = partition_district_graph(smd_graph, district_config)
+        nx.draw(district_partition, pos=smd_centroids, node_size=[10]*len(smd_graph.nodes))
         plt.show()
         plt.clf()
 
