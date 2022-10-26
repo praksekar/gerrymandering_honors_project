21acea211b09d841b853a47a3b2703ce28479ef9
Prakash Sekar <pkhsekar@gmail.com>
2022-10-26 18:43:31 -0400

    about to create MMD_Partition class

--- mmds.py
+++ mmds.py
@@ -9,22 +9,27 @@ import networkx as nx
 from geopandas import GeoSeries
 from gerrychain import Graph, MarkovChain, Partition, constraints
 from gerrychain.accept import always_accept
-from gerrychain.tree import random_spanning_tree
 from gerrychain.updaters import Tally, cut_edges
 from matplotlib.colors import ListedColormap
 from pptx import Presentation
+from typing import Callable
 
 
 SHAPE_FILE = "./state_data/PA/PA.shp"
 GRAPH_FILE = "./PAjson_wo_geometry"
 PRES_FILE = "./pres_output/mmd_recom2"
 SMD_ASSIGNMENT_COL = "CD_2011"
-PLOT_INTERVAL = 50
+PLOT_INTERVAL = 1 
 POP_COL = "TOTPOP"
 COLORS = ListedColormap(['#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231',
 '#911eb4', '#46f0f0', '#f032e6', '#bcf60c', '#fabebe', '#008080', '#e6beff',
 '#9a6324', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000075',
 '#808080'])
+
+class MMD_Partition(Partition):
+    def __init__(self, graph=None, assignment=None, updaters=None, parent=None, flips=None, use_default_updaters=True):
+        super().__init__(self, graph, assignment, updaters, parent, flips, use_default_updaters)
+        self.
 
 
 def add_plot_to_pres(prs):
@@ -35,6 +40,7 @@ def add_plot_to_pres(prs):
 
 
 def plot_partition(partition: Partition, precinct_geometries: GeoSeries, prs, show=False, cmap=COLORS):
+    # plt.title("Markov chain step: %d" % chain_step)
     partition.plot(precinct_geometries, cmap=cmap)
     centroids : dict[int, tuple] = get_district_centroids(partition, precinct_geometries)
     for partID, coord in centroids.items():
@@ -50,16 +56,13 @@ def count_components(G: nx.Graph) -> int:
     return len([c for c in nx.connected_components(G)])
 
 
-recursive_calls = 0
-def rec(graph, tree: Graph, curr_node: int, parent: int, graph_pop: int, pop_rng: tuple) -> tuple:
-    global recursive_calls
-    recursive_calls += 1
+def find_cut(graph, tree: Graph, curr_node: int, parent: int, graph_pop: int, pop_rng: tuple) -> tuple:
     sum = graph.nodes[curr_node][POP_COL] 
     for child in tree.neighbors(curr_node):
         if child != parent:
-            child_sum, found_edge = rec(graph, tree, child, curr_node, graph_pop, pop_rng) 
-            if found_edge is not None:
-                return (None, found_edge)
+            child_sum, edge_found = find_cut(graph, tree, child, curr_node, graph_pop, pop_rng) 
+            if edge_found:
+                return (None, edge_found)
             sum += child_sum
     if pop_rng[0] <= sum <= pop_rng[1] or pop_rng[0] <= graph_pop-sum <= pop_rng[1]:
         return (None, (curr_node, parent))
@@ -67,27 +70,23 @@ def rec(graph, tree: Graph, curr_node: int, parent: int, graph_pop: int, pop_rng
 
 
 def split_graph_by_pop(graph: nx.Graph, pop_target: int, graph_pop: int, epsilon: float, node_repeats: int = 30) -> tuple[list[int]]:
-    global recursive_calls
-    recursive_calls = 0
     pop_rng = (pop_target * (1 - epsilon), pop_target * (1 + epsilon))
     graph_edges = list(graph.edges)
     for i in range(node_repeats):
         with CodeTimer("create spanning tree"):
-            spanning_tree = gen_random_spanning_tree(graph, graph_edges)
+            spanning_tree = rand_spanning_tree(graph, graph_edges)
         root = random.choice(list(spanning_tree.nodes))
-        with CodeTimer("dfs to find cut edge"):
-            sum, cut_edge = rec(graph, spanning_tree, root, None, graph_pop, pop_rng)
-        if cut_edge is not None:
-            print("finished recom after %d random spanning trees and %d recursive calls on %d nodes" % (i+1, recursive_calls, len(spanning_tree.nodes)))
+        sum, cut_edge = find_cut(graph, spanning_tree, root, None, graph_pop, pop_rng)
+        if cut_edge:
+            print("finished recom after %d random spanning trees on %d nodes" % (i+1, len(spanning_tree.nodes)))
             spanning_tree.remove_edge(cut_edge[0], cut_edge[1])
-            with CodeTimer("get left and right components"):
-                comp_1 = [node for node in nx.dfs_postorder_nodes(spanning_tree, source=cut_edge[0])]
-                comp_2 = [node for node in nx.dfs_postorder_nodes(spanning_tree, source=cut_edge[1])]
+            comp_1 = [node for node in nx.dfs_postorder_nodes(spanning_tree, source=cut_edge[0])]
+            comp_2 = [node for node in nx.dfs_postorder_nodes(spanning_tree, source=cut_edge[1])]
             return(comp_1, comp_2)
     raise Exception("partitioning failed")
 
 
-def gen_random_spanning_tree(graph: nx.Graph, edges):
+def rand_spanning_tree(graph: nx.Graph, edges):
     spanning_forest = nx.Graph()
     spanning_forest.add_nodes_from(graph.nodes)
     random.shuffle(edges)
@@ -141,7 +140,7 @@ def gen_smd_graph(partition: Partition) -> nx.Graph:
 
 def cut_smd_graph(graph: nx.Graph, mmd_config: tuple, cut_iterations: int = 100000, node_repeats: int = 20) -> nx.Graph:
     for _ in range(node_repeats):
-        spanning_tree: nx.Graph = nx.random_spanning_tree(graph)
+        spanning_tree: nx.Graph = rand_spanning_tree(graph, list(graph.edges))
         for _ in range(cut_iterations):
             random_edges: list[tuple] = random.sample(list(spanning_tree.edges), sum(mmd_config)-1)
             spanning_tree.remove_edges_from(random_edges)
@@ -165,6 +164,30 @@ def gen_mmd_seed_assignment(smd_partition: Partition) -> dict[int, int]:
     return assignment
 
 
+def gen_random_map(chain: MarkovChain) -> Partition:
+    for partition in chain:
+        partition.test_field = 3
+        continue
+    return partition
+
+
+def gen_ensemble(chain: MarkovChain, num_maps) -> list[Partition]:
+    return [gen_random_map(chain) for _ in range(num_maps)]
+
+
+# def precinct_ranked_choice(district: Partition, precinctID: int) -> list:
+    
+
+
+# def ranked_choice_election(partition: Partition, voting_function: Callable[[]] = party_line_voting)
+
+# def memoize_markov_chain(markov_chain: MarkovChain):
+
+
+
+
+
+
 def main() -> None:
     prs = Presentation()
 
@@ -183,24 +206,30 @@ def main() -> None:
         [],
         always_accept,
         mmd_partition,
-        total_steps=500
+        total_steps=10
     )
 
-    try:
-        prev_time = time.process_time_ns()
-        for stepno, partition in enumerate(chain):
-            curr_time = time.process_time_ns()
-            print("chain step total time: %d ms" % (float(curr_time-prev_time)/1000000))
-            prev_time = curr_time
-            print("step: %d" % stepno)
-            print("partition components: %d" % count_components(partition.graph))
-            if stepno % PLOT_INTERVAL == 0:
-                plot_partition(partition, prec_geometries, prs=prs, show=False)
-    except KeyboardInterrupt:
-        print("keyboard interrupt")
-    finally:
-        prs.save("%s" % PRES_FILE)
-        print("done, saved as %s" % PRES_FILE)
+    ensemble: list[Partition] = gen_ensemble(chain, 10)
+    for partition in ensemble:
+        plot_partition(partition, prec_geometries, prs, show=False)
+    prs.save("%s" % PRES_FILE)
+    print("done, saved as %s" % PRES_FILE)
+
+    # try:
+    #     prev_time = time.process_time_ns()
+    #     for stepno, partition in enumerate(chain):
+    #         curr_time = time.process_time_ns()
+    #         print("chain step total time: %d ms" % (float(curr_time-prev_time)/1000000))
+    #         prev_time = curr_time
+    #         print("step: %d" % stepno)
+    #         print("partition components: %d" % count_components(partition.graph))
+    #         # if stepno % PLOT_INTERVAL == 0:
+    #         #     plot_partition(partition, prec_geometries, prs, stepno, show=False)
+    # except KeyboardInterrupt:
+    #     print("keyboard interrupt")
+    # finally:
+    #     prs.save("%s" % PRES_FILE)
+    #     print("done, saved as %s" % PRES_FILE)
 
 
 if __name__ == "__main__":
